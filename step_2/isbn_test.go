4dc1b624f755d6771f8ab1c40f91b6e30077f7f6
Jakub Gruszecki <gruszecki.jakub@gmail.com>
2024-05-20 21:34:04 +0200

    small refactors

--- step_2/isbn_test.go
+++ step_2/isbn_test.go
@@ -2,54 +2,48 @@ package step_2
 
 import "testing"
 
-func Test_ValidateISBN(t *testing.T) {
-	type args struct {
-		isbn ISBN
-	}
+func Test_ISBN_Validate(t *testing.T) {
 	tests := []struct {
 		name string
-		args args
+		isbn ISBN
 		want bool
 	}{
 		// ISBN 83-7054-061-9
 		// Sapkowski, Andrzej. Opowieści o Wiedźminie.  T.1.  Ostatnie życzenie /  Andrzej Sapkowski. Warszawa: Super Nowa, 2004. Print."
-		{"valid isbn 10", args{ISBN{8, 3, 7, 0, 5, 4, 0, 6, 1, 9}}, true},
-		{"invalid isbn 10", args{ISBN{8, 3, 7, 0, 5, 4, 0, 6, 1, 7}}, false},
+		{"valid isbn 10", ISBN{8, 3, 7, 0, 5, 4, 0, 6, 1, 9}, true},
+		{"invalid isbn 10", ISBN{8, 3, 7, 0, 5, 4, 0, 6, 1, 7}, false},
 
 		// ISBN 978-83-7578-063-5
 		// Sapkowski, Andrzej. Ostatnie życzenie / Andrzej Sapkowski. Wydanie 1 w tej edycji. Warszawa: SuperNowa, 2014. Print.
-		{"valid isbn 13", args{ISBN{9, 7, 8, 8, 3, 7, 5, 7, 8, 0, 6, 3, 5}}, true},
-		{"invalid isbn 13", args{ISBN{9, 7, 8, 8, 3, 7, 5, 7, 8, 0, 6, 3, 3}}, false},
+		{"valid isbn 13", ISBN{9, 7, 8, 8, 3, 7, 5, 7, 8, 0, 6, 3, 5}, true},
+		{"invalid isbn 13", ISBN{9, 7, 8, 8, 3, 7, 5, 7, 8, 0, 6, 3, 3}, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.args.isbn.Validate(); got != tt.want {
+			if got := tt.isbn.Validate(); got != tt.want {
 				t.Errorf("isbn.Validate() = %v, want %v", got, tt.want)
 			}
 		})
 	}
 }
 
-func TestCalculateISBNCheckDigit(t *testing.T) {
-	type args struct {
-		isbn ISBN
-	}
+func Test_ISBN_CalculateCheckDigit(t *testing.T) {
 	tests := []struct {
 		name string
-		args args
+		isbn ISBN
 		want int
 	}{
 		// ISBN 83-7054-061-9
 		// Sapkowski, Andrzej. Opowieści o Wiedźminie.  T.1.  Ostatnie życzenie /  Andrzej Sapkowski. Warszawa: Super Nowa, 2004. Print."
-		{"calculate isbn 10 check digit", args{ISBN{8, 3, 7, 0, 5, 4, 0, 6, 1}}, 9},
+		{"calculate isbn 10 check digit", ISBN{8, 3, 7, 0, 5, 4, 0, 6, 1}, 9},
 
 		// ISBN 978-83-7578-063-5
 		// Sapkowski, Andrzej. Ostatnie życzenie / Andrzej Sapkowski. Wydanie 1 w tej edycji. Warszawa: SuperNowa, 2014. Print.
-		{"calculate isbn 13 check digit", args{ISBN{9, 7, 8, 8, 3, 7, 5, 7, 8, 0, 6, 3}}, 5},
+		{"calculate isbn 13 check digit", ISBN{9, 7, 8, 8, 3, 7, 5, 7, 8, 0, 6, 3}, 5},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.args.isbn.CalculateCheckDigit(); got != tt.want {
+			if got := tt.isbn.CalculateCheckDigit(); got != tt.want {
 				t.Errorf("isbn.CalculateCheckDigit() = %v, want %v", got, tt.want)
 			}
 		})
